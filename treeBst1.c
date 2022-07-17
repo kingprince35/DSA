9fc18b07ec4326839f3379f9696a75d1c3a0f225
tejas <tejasshah@gmail.com>
2022-07-17 12:30:19 +0530

    tree travel

--- treeBst1.c
+++ treeBst1.c
@@ -34,21 +34,57 @@ struct node* addNode(int num,struct node *root){//100,(50) | 100,NULL
         return root;
 }
 
+void inOrder(struct node *root){
+    if(root == NULL)
+        return;
+    inOrder(root->left);
+    printf("%d ",root->data);
+    inOrder(root->right);
+}
+
+
+void preOrder(struct node *root){
+    if(root == NULL)
+        return;
+    printf("%d ",root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void postOrder(struct node *root){
+    if(root == NULL)
+        return;
+    postOrder(root->left);
+    postOrder(root->right);
+    printf("%d ",root->data);
+
+}
+
+
 
 int main(){
     int i=1;
 
-    root = addNode(50,root);//NULL
+/*    root = addNode(50,root);//NULL
            addNode(100,root);//50
             addNode(35,root);
             addNode(25,root);
             addNode(40,root);
             addNode(150,root);
             addNode(250,root);
+*/
+        root=addNode(50,root);
+            addNode(35,root);
+            addNode(25,root);
+            addNode(40,root);
+            addNode(100,root);
+            addNode(80,root);
+            addNode(150,root);
 
             //1 for add
             //2 for print
             //print(root);
+    /*
     printf("\n%d ",root->data);//50
     printf("\n%d ",root->left->data);//35
     printf("\n%d ",root->left->left->data);//25
@@ -56,8 +92,31 @@ int main(){
     printf("\n%d ",root->right->data);//100
     printf("\n%d ",root->right->right->data);//150
     printf("\n%d ",root->right->right->right->data);//250
-
+    */
+    printf("\nIn Order\n");
+    inOrder(root);
+    printf("\nPre Order\n");
+    preOrder(root);
+    printf("\nPost Order\n");
+    postOrder(root);
 
 
     return 0;
 }
+/*
+
+        Tree Travel :-
+
+                    Pre Order ->    Root-Left-Right
+                    In  Order ->    Left-Root-Right
+                    Post Order ->   Left-Right->Root
+
+                    Pre -> 50 , 35 , 25 , 40 , 100, 150,250
+                    In  -> 25,35,40,50,100,150,250 [ asc ]
+                    Post-> 25,40,35,
+
+
+
+
+
+*/
